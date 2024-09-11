078c182b6d4c32fc4ec102bb60c0df0970f48313
jungminji <wjdalswl0215@gmail.com>
2024-09-11 11:48:06 +0900

    no-refactoring 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = useQueryClient();
+
+  const [title, setTitle] = useState("");
+  const [views, setViews] = useState("");
+
+  const getPosts = async () => {
+    const { data } = await axios.get("http://localhost:4000/posts");
+    console.log("data :>> ", data);
+    return data;
+  };
+
+  const {
+    data: posts,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
+  const addPost = async (post) => {
+    await axios.post("http://localhost:4000/posts", post);
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: addPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  const handleAddPost = (e) => {
+    e.preventDefault();
+
+    const post = {
+      title: title,
+      views: views,
+    };
+
+    mutate(post);
+  };
+
+  if (isPending) return <div>로딩중입니다...</div>;
+
+  if (isError) return <div>오류가 발생하였습니다...</div>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleAddPost}>
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <input
+          value={views}
+          onChange={(e) => {
+            setViews(e.target.value);
+          }}
+        />
+        <button type={"submit"}>추가</button>
+      </form>
+
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <span>{post.views}</span>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
